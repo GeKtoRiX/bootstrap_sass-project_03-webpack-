86ccdd5e19a0156ce2a9be37838459f3d54a4d24
cb@ndy <gektor93@gmail.com>
2022-05-24 20:56:34 +0300

    home course features resources

---
+++ README.txt
@@ -0,0 +1,14 @@
+…or create a new repository on the command line
+
+echo "# bootstrap_sass-project_03-webpack-" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/GeKtoRiX/bootstrap_sass-project_03-webpack-.git
+git push -u origin main
+
+…or push an existing repository from the command line
+git remote add origin https://github.com/GeKtoRiX/bootstrap_sass-project_03-webpack-.git
+git branch -M main
+git push -u origin main
--- src/index.js
+++ src/index.js
@@ -5,11 +5,31 @@ var reqScripts = require.context("./scripts", true, /^(.*\.(js$))[^.]*$/i);
 reqScripts.keys().forEach(reqScripts);
 
 // projects icons
-import navIconSrc from './icons/site-logo.png';
+import navIconSrc from './icons/nav-bar/site-logo.png';
+import featureIconSrc1 from './icons/features/play-img.png';
+import featureIconSrc2 from './icons/features/slider-img.png';
+import featureIconSrc3 from './icons/features/form-img.png';
+import resourcesIconSrc1 from './icons/resources/html-img.png';
+import resourcesIconSrc2 from './icons/resources/bootstrap-img.png';
+import resourcesIconSrc3 from './icons/resources/css-img.png';
 // projects imgs
 
 // #nav-icon
 const navIcon = document.getElementById('logo-site');
 navIcon.src = navIconSrc;
+// #features
+const featureIcon1 = document.getElementById('featureIcon1');
+featureIcon1.src = featureIconSrc1 ;
+const featureIcon2 = document.getElementById('featureIcon2');
+featureIcon2.src = featureIconSrc2;
+const featureIcon3 = document.getElementById('featureIcon3');
+featureIcon3.src = featureIconSrc3;
+// #resources
+const resourcesIcon1 = document.getElementById('resourcesIcon1');
+resourcesIcon1.src = resourcesIconSrc1;
+const resourcesIcon2 = document.getElementById('resourcesIcon2');
+resourcesIcon2.src = resourcesIconSrc2;
+const resourcesIcon3 = document.getElementById('resourcesIcon3');
+resourcesIcon3.src = resourcesIconSrc3;
 
 
